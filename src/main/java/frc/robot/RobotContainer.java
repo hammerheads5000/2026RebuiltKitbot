0374376ea264fea32b13f3b21bb42ca55097a3b1
Luc Mondon <lucjmondon@gmail.com>
2026-02-27 13:26:01 -0500

    Update RobotContainer.java
    added a chooser so we can pick auto in smart dashboard and redid RobotContainer so it would work with swerve

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,45 +5,70 @@
 package frc.robot;
 
 import static edu.wpi.first.units.Units.RPM;
+import static edu.wpi.first.units.Units.MetersPerSecond;
+import static edu.wpi.first.units.Units.RadiansPerSecond;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-import frc.robot.subsystems.Drive;
+import com.ctre.phoenix6.swerve.SwerveRequest;
+import frc.robot.subsystems.CommandSwerveDrivetrain;
+import frc.robot.Constants.SwerveConstants;
 import frc.robot.subsystems.FuelMechanism;
-import frc.robot.subsystems.TeleopDrive;
 
 public class RobotContainer {
-  Drive drive = new Drive();
+  
+  public final CommandSwerveDrivetrain drivetrain = new CommandSwerveDrivetrain(
+      SwerveConstants.DRIVETRAIN_CONSTANTS,
+      SwerveConstants.FrontLeft.MODULE_CONSTANTS,
+      SwerveConstants.FrontRight.MODULE_CONSTANTS,
+      SwerveConstants.BackLeft.MODULE_CONSTANTS,
+      SwerveConstants.BackRight.MODULE_CONSTANTS
+  );
+
   FuelMechanism fuelMechanism = new FuelMechanism();
   CommandXboxController controller = new CommandXboxController(0);
-  TeleopDrive teleopDrive = new TeleopDrive(drive, controller);
+
+  private final SendableChooser<Command> autoChooser; 
+
+  private final SwerveRequest.FieldCentric swerveDrive = new SwerveRequest.FieldCentric()
+      .withDeadband(SwerveConstants.LINEAR_VEL_DEADBAND.in(MetersPerSecond))
+      .withRotationalDeadband(SwerveConstants.ANGLULAR_VEL_DEADBAND.in(RadiansPerSecond))
+      .withDriveRequestType(SwerveConstants.DRIVE_REQUEST_TYPE);
+
   Trigger farShootTrigger = controller.rightTrigger();
   Trigger nearShootTrigger = controller.rightBumper();
   Trigger intakeTrigger = controller.leftTrigger();
-  Trigger driveForwardTrigger = controller.a();
-  Trigger driveBackwardTrigger = controller.b();
-  Trigger cSpinTrigger  =  controller.y(); 
-  Trigger ccSpinTrigger  =  controller.x();
+  Trigger resetGyroTrigger = controller.y();
 
   public RobotContainer() {
-    drive.setDefaultCommand(teleopDrive);
+    autoChooser = com.pathplanner.lib.auto.AutoBuilder.buildAutoChooser();
+    SmartDashboard.putData("Auto Chooser", autoChooser);
+
     configureBindings();
+
+    drivetrain.setDefaultCommand(
+        drivetrain.applyRequest(() -> swerveDrive
+            .withVelocityX(-controller.getLeftY() * SwerveConstants.FAST_DRIVE_SPEED.in(MetersPerSecond)) 
+            .withVelocityY(-controller.getLeftX() * SwerveConstants.FAST_DRIVE_SPEED.in(MetersPerSecond)) 
+            .withRotationalRate(-controller.getRightX() * SwerveConstants.FAST_ROT_SPEED.in(RadiansPerSecond))
+        )
+    );
   }
 
   private void configureBindings() {
-    driveForwardTrigger.whileTrue(drive.driveForwardCommand(0.4));
-    driveBackwardTrigger.whileTrue(drive.driveBackwardCommand(0.4));
-    cSpinTrigger.whileTrue(drive.spinCommand(0.7));
-    ccSpinTrigger.whileTrue(drive.spinCommand(-0.7));
     farShootTrigger.whileTrue(fuelMechanism.shootCommand(RPM.of(4350)));
     nearShootTrigger.whileTrue(fuelMechanism.shootCommand(RPM.of(3700)));
     intakeTrigger.whileTrue(fuelMechanism.intakeCommand(RPM.of(4000)));
 
+    resetGyroTrigger.onTrue(drivetrain.runOnce(() -> drivetrain.resetPose(new Pose2d(0, 0, new Rotation2d(0)))));
   }
 
   public Command getAutonomousCommand() {
-    return Commands.print("No autonomous command configured");
+    return autoChooser.getSelected();
   }
 }
